e203aa6f15dc2ae3f76763f2c6315d96fd71a910
James Hurst <james_hurst@byu.edu>
2020-04-16 22:39:36 +0000

    comment out redacted fields options

--- modules/waf-regional/main.tf
+++ modules/waf-regional/main.tf
@@ -137,16 +137,16 @@ resource "aws_wafregional_web_acl" "wafregional_acl" {
    logging_configuration {
     log_destination = var.log_destination_arn
 
-    redacted_fields {
-      field_to_match {
-        type = "URI"
-      }
+    #redacted_fields {
+      #field_to_match {
+        #type = "URI"
+      #}
 
-      field_to_match {
-        data = "referer"
-        type = "HEADER"
-      }
-    }
+      #field_to_match {
+        #data = "referer"
+        #type = "HEADER"
+      #}
+    #}
   }
 }
 
